0a6056363b6345a5c8e8b0710beae27558bba289
walkingway <walkingway@126.com>
2014-05-05 23:14:03 +0800

    Implement The mini-harp Command

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2));
 var createMiniHarp = require("mini-harp")
   , app = createMiniHarp();
-console.log("Starting mini-harp on http://localhost:4000");
-app.listen(4000);
+var num = argv["port"];
+if (num){
+  console.log("Starting mini-harp on http://localhost:%d", num);
+  app.listen(num);
+} else {
+  console.log("Starting mini-harp on http://localhost:4000");
+  app.listen(4000);
+}
