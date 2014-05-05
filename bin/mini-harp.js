c7f465907d94e978d3204d6b0f018855753e854e
walkingway <walkingway@126.com>
2014-05-06 01:48:40 +0800

    Serving Static Content

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
 var argv = require('minimist')(process.argv.slice(2));
+root = argv._[0] || process.cwd();
+
 var createMiniHarp = require("mini-harp")
-  , app = createMiniHarp();
-app.use(function (req, res, next) {
-      if (req.url === '/current-time') {
-        res.end((new Date()).toISOString());
-      } else {
-        next();
-      }
-    });
-var num = argv["port"];
-if (num){
-  console.log("Starting mini-harp on http://localhost:%d", num);
-  app.listen(num);
-} else {
-  console.log("Starting mini-harp on http://localhost:4000");
-  app.listen(4000);
-}
+  , app = createMiniHarp(root);
+
+var num = argv["port"] || 4000;
+
+console.log("Starting mini-harp on http://localhost:%d" + root, num);
+app.listen(num);
+
+
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var connect = require("connect");
+var serveStatic = require('serve-static');
 
-module.exports = function() {
+module.exports = function(root) {
   var app = connect();
+  app.use(serveStatic(root))
+     .use(function (req, res, next) {
+        if (req.url == '/current-time') {
+          res.end((new Date()).toISOString());
+        } else {
+          next();
+        }
+      });
   return app;
 }
